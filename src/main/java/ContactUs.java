58e93afa22d529beffb867c58b014108647287e8
Stephane KEOU <kakmenikeou@gmail.com>
2021-06-02 15:52:41 +0200

    Women, MyAddresses, SearchResult et ContactUs pages

---
+++ src/main/java/ContactUs.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ContactUs {
+    WebDriver driver;
+    WebDriverWait wait;
+
+    public ContactUs(WebDriver driver){
+        this.driver=driver;
+    }
+    By headerLogoSelector =By.cssSelector("#header_logo >a>img");
+
+    public void backToHomePageByLogo(){
+        wait= new WebDriverWait(driver,50);
+        wait.until(ExpectedConditions.elementToBeClickable(headerLogoSelector));
+        driver.findElement(headerLogoSelector).click();
+    }
+
+
+}
